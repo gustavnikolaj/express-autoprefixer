gustavnikolaj/express-autoprefixer

commit 533f2853c1b95244e621f531fde86cd58ebc9976

add test for 304's and fix it

lib/autoprefixer.js
@@ -6,6 +6,7 @@ module.exports = function () {
     var args = Array.prototype.slice.call(arguments);
 
     return interceptor(function (req, res) {
+        var fileType = (req.originalUrl.match(/\.(le|c)ss$/) || []).shift();
         // Prevent If-None-Match revalidation with the downstream middleware with ETags that aren't suffixed with "-autoprefixer":
         var ifNoneMatch = req.headers['if-none-match'];
         if (ifNoneMatch) {
@@ -25,33 +26,46 @@ module.exports = function () {
 
         return {
             isInterceptable: function () {
-                return /text\/css/.test(res.getHeader('Content-Type'));
+                // the content-type header is not set on 304 responses, and we need to
+                // intercept those as well, in order to patch the etag header (the
+                // -autoprefixer suffix). that's why we use the fileType check.
+                // non-matching file types will still be autoprefixed if they have
+                // the right content-type, but they will not be 304-able.
+                return !!fileType || /text\/css/.test(res.getHeader('Content-Type'));
             },
             intercept: function (body, send) {
                 var contentType = res.getHeader('Content-Type');
                 var matchContentType = contentType && contentType.match(/^text\/css(?:;\s*charset=([a-z0-9\-]+))?$/i);
+                var upstreamETag;
 
-                // The mime module doesn't support less yet, so we fall back:
-                if (matchContentType) {
-                    if (!body.length) {
-                        send(res.statusCode);
+                if (res.statusCode === 304) {
+                    upstreamETag = res.getHeader('ETag');
+                    if (upstreamETag && !(/-autoprefixer"$/.test(upstreamETag))) {
+                        res.setHeader('ETag', upstreamETag.replace(/"$/, '-autoprefixer"'));
                     }
+                    return send(body);
+                } else if (res.statusCode === 200) {
+                    // The mime module doesn't support less yet, so we fall back:
+                    if (matchContentType) {
+                        if (!body.length) {
+                            send(res.statusCode);
+                        }
 
-                    postcss([autoprefixer.apply(null, args)])
-                        .process(body)
-                        .then(function (result) {
-                            res.setHeader('Content-Type', 'text/css');
-                            var upstreamETag = res.getHeader('ETag');
-                            if (upstreamETag) {
-                                res.setHeader('ETag', upstreamETag.replace(/"$/, '-autoprefixer"'));
-                            }
-                            res.setHeader('Content-Length', Buffer.byteLength(result.css));
-                            send(result.css);
-                        })
-                        .catch(function (err) {
-                            send(500, err);
-                        });
-
+                        postcss([autoprefixer.apply(null, args)])
+                            .process(body)
+                            .then(function (result) {
+                                res.setHeader('Content-Type', 'text/css');
+                                upstreamETag = res.getHeader('ETag');
+                                if (upstreamETag) {
+                                    res.setHeader('ETag', upstreamETag.replace(/"$/, '-autoprefixer"'));
+                                }
+                                res.setHeader('Content-Length', Buffer.byteLength(result.css));
+                                send(result.css);
+                            })
+                            .catch(function (err) {
+                                send(500, err);
+                            });
+                    }
                 } else {
                     send(body);
                 }

test/autoprefixer.spec.js
@@ -92,4 +92,50 @@ describe('express-autoprefixer', function () {
             content: '<!DOCTYPE html><html></html>'
         }, 'to be served as', '<!DOCTYPE html><html></html>');
     });
+    describe('unexpected-fs tests', function () {
+        var expect = require('unexpected')
+            .clone()
+            .installPlugin(require('unexpected-fs'))
+            .installPlugin(require('unexpected-express'))
+            .addAssertion('to yield response', function (expect, subject, value) {
+                var browsers = subject.browsers || 'Chrome > 30';
+                var app = express()
+                    .use(autoprefixer({ browsers: browsers, cascade: false }))
+                    .use(express['static']('/data'));
+                return expect(app, 'to yield exchange', {
+                    request: subject,
+                    response: value
+                });
+            });
+        it('should allow a request', function () {
+            var mockFs = {
+                '/data': {
+                    'foobar.css': {
+                        _isFile: true,
+                        mtime: new Date(1),
+                        content: '.foo { animation-name: bar; }'
+                    }
+                }
+            };
+            return expect('/foobar.css', 'with fs mocked out', mockFs, 'to yield response', {
+                statusCode: 200,
+                headers: {
+                    ETag: /^W\/".*-autoprefixer"$/
+                }
+            }).then(function (context) {
+                var etag = context.httpResponse.headers.get('ETag');
+                return expect({
+                    url: '/foobar.css',
+                    headers: {
+                        'If-None-Match': etag
+                    }
+                }, 'with fs mocked out', mockFs, 'to yield response', {
+                    statusCode: 304,
+                    headers: {
+                        ETag: etag
+                    }
+                });
+            });
+        });
+    });
 });

commit b73ddb6547646954180ad18c8a89ac8652791d4a

Add support for autoprefixing css output from a sass compiler down the pipeline. Regressed in https://github.com/gustavnikolaj/express-autoprefixer/commit/533f2853c1b95244e621f531fde86cd58ebc9976#diff-f517ccf3cfb4fdf47a37752031b060aaR9

lib/autoprefixer.js
@@ -17,7 +17,7 @@ module.exports = function expressAutoprefixer() {
     const contentTypeCache = new LRU(lruMaxItems);
 
     return (req, res, next) => {
-        const fileType = (req.originalUrl.split('?')[0].match(/\.(le|c)ss$/) || []).shift();
+        const fileType = (req.originalUrl.split('?')[0].match(/\.(le|c|sc)ss$/) || []).shift();
         const cachedContentType = contentTypeCache.get(req.originalUrl);
 
         // Attempt to load the content type for this URL from the cache. If we

test/autoprefixer.spec.js
@@ -122,6 +122,20 @@ describe('express-autoprefixer', () => {
         );
     });
 
+    it('should work with scss files served through express-compile-sass', () => {
+        // express-compile-sass will compile .scss files on the fly and serve the
+        // compiled content with content-type text/css on the original url.
+        return expect(
+            {
+                url: '/style.scss',
+                contentType: 'text/css',
+                content: '.foo { animation: bar; }'
+            },
+            'to be served as',
+            '.foo { -webkit-animation: bar; animation: bar; }'
+        );
+    });
+
     it('should serve html without throwing errors', () => {
         return expect(
             {